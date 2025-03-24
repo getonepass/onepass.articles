1ed9f29ef6c4a6a0be11132216f6fc0d99060f6f
AEscrich <almudena.escrich@fundingbox.com>
2025-03-24 20:11:22 +0100

    initial input workspaces 20250324

---
+++ _meta.tsx
@@ -0,0 +1,35 @@
+import type { MetaRecord } from "nextra"
+export default {
+  "getting-started": {
+    title: "Getting started",
+    display: "hidden",
+  },
+  "account": {
+    title: "User Account",
+    display: "hidden",
+  },
+  "teams-workspaces": {
+    title: "Workspaces and Team Members",
+    display: "hidden",
+  },
+  "document-management": {
+    title: "Managing and Sharing Documents",
+    display: "hidden",
+  },
+  "lists": {
+    title: "Creating and Managing Lists",
+    display: "hidden",
+  },
+  "emails-and-messaging": {
+    title: "Communicating through OnePass",
+    display: "hidden",
+  },
+  "Opportunities": {
+    title: "Identifying further opportunities",
+    display: "hidden",
+  },
+  "faq": {
+    title: "Frequently Asked Questions",
+    display: "hidden",
+  },
+} satisfies MetaRecord
