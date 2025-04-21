e53c1c5245c6c9ec632c7d31a15f38ba47872f2a
Jorge Fernández <jorge.fernandez@cores.gl>
2025-04-22 00:43:31 +0200

    update article metadata and add support articles index page

--- _meta.tsx
+++ _meta.tsx
@@ -6,30 +6,23 @@ export default {
   },
   account: {
     title: "User Account",
-    display: "hidden",
   },
   "teams-workspaces": {
     title: "Workspaces and Team Members",
-    display: "hidden",
   },
   "documents-management": {
     title: "Managing and Sharing Documents",
-    display: "hidden",
   },
   lists: {
     title: "Creating and Managing Lists",
-    display: "hidden",
   },
   "emails-and-messaging": {
     title: "Communicating through OnePass",
-    display: "hidden",
   },
   opportunities: {
     title: "Identifying further opportunities",
-    display: "hidden",
   },
   faqs: {
     title: "Frequently Asked Questions",
-    display: "hidden",
   },
 } satisfies MetaRecord
